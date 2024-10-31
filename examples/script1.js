88ab845c69e8af5df660de53324b96caff2c2e48
joaofavoretti <contas.joaopedro.favoretti@gmail.com>
2024-10-31 16:26:43 -0300

    Cleaning repository

---
+++ examples/script1.js
@@ -0,0 +1,14 @@
+
+function script1_foo() {
+    const element = document.getElementById('myElement');
+    element.innerHTML = 'script1_foo executed!';
+}
+
+function script1_bar() {
+    const url = window.location.href;
+    const userAgent = navigator.userAgent;
+    const platform = navigator.platform;
+    const deviceMemory = navigator.deviceMemory;
+}
+
+
---
+++ examples/script2.js
@@ -0,0 +1,7 @@
+(() => {
+    const url = window.location.href;
+    const userAgent = navigator.userAgent;
+    const platform = navigator.platform;
+    const deviceMemory = navigator.deviceMemory;
+})()
+
